d85cef4b92aa11142e7a9d2f3ae9a1ea89b1d0e5
Richard Schneider <richard@schneiderbox.net>
2014-03-22 08:42:12 -0500

    Add frame calls

--- js/game.js
+++ js/game.js
@@ -1,11 +1,31 @@
-function Game(){
-	this.canvas = document.getElementById('snakeCanvas')
-	this.ctx = this.canvas.getContext('2d')
+function Game(canvasID){
+	this.canvas = document.getElementById(canvasID);
+	this.ctx = this.canvas.getContext('2d');
 
 	this.ctx.fillStyle = '#fff';
 	this.ctx.fillRect(0, 0, 500, 500);
 
 	this.world = new World();
+	this.world.game = this;
+
+	this.lastTime = 0;
+	var game = this;
+	window.requestAnimationFrame(function(time){
+		game.frame(time);
+	});
 }
 
-var game = new Game();
+Game.prototype.frame = function(time){
+	var delta = time - this.lastTime;
+	this.lastTime = time;
+	
+	this.world.update(delta);
+	//this.world.render(this.ctx);
+
+	var game = this;
+	window.requestAnimationFrame(function(time){
+		game.frame(time);
+	});
+}
+
+var game = new Game('snakeCanvas');
