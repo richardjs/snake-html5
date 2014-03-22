030d8e63fcb6136fc5dcacd531a63122f8befa1d
Richard Schneider <richard@schneiderbox.net>
2014-03-22 14:18:23 -0500

    Add ticking logic to the world

--- js/world.js
+++ js/world.js
@@ -1,5 +1,8 @@
 function World(){
 	this.entityGroups = {};
+
+	this.tickLength = 1000
+	this.tickTimer = 1000	
 }
 
 World.prototype.addEntity = function(entity, group){
@@ -11,16 +14,20 @@ World.prototype.addEntity = function(entity, group){
 }
 
 World.prototype.update = function(delta){
-	for(var group in this.entityGroups){
-		var entities = this.entityGroups[group];
-		for(var i = 0; i < entities.length; i++){
-			var entity = entities[i];
-			entity.update(delta);
+	this.tickTimer += delta;
+	var ticked = false;
+	while(this.tickTimer >= this.tickLength){
+		this.tickTimer -= this.tickLength;
+		for(var group in this.entityGroups){
+			var entities = this.entityGroups[group];
+			for(var i = 0; i < entities.length; i++){
+				var entity = entities[i];
+				entity.update(delta);
+			}
 		}
+		ticked = true;
 	}
-	
-	//TODO: only return true on a tick
-	return true
+	return ticked;
 }
 
 World.prototype.render = function(ctx){
